dd7e14ceaa032eb59240cbefadeb76d729fadbb4
Nikishin Dmitry <dmitry.nikishin@technomos.ru>
2023-08-18 00:35:18 +0300

    vars renamed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -24,14 +24,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	fullUrl := string(bodyData)
-	_, err = url.ParseRequestURI(fullUrl)
+	fullURL := string(bodyData)
+	_, err = url.ParseRequestURI(fullURL)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	if shortUrl, ok := cache[fullUrl]; ok {
-		w.Write([]byte(shortUrl))
+	if shortURL, ok := cache[fullURL]; ok {
+		w.Write([]byte(shortURL))
 		return
 	}
 	hash, err := hasher.Write(bodyData)
@@ -40,26 +40,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	shortUrl, err := sc.Encode([]uint64{uint64(hash)})
-	if _, ok := cacheBack[shortUrl]; ok || err != nil {
+	shortURL, err := sc.Encode([]uint64{uint64(hash)})
+	if _, ok := cacheBack[shortURL]; ok || err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	shortUrl = "http://localhost:8080/" + shortUrl
-	cache[fullUrl] = shortUrl
-	cacheBack[shortUrl] = fullUrl
+	shortURL = "http://localhost:8080/" + shortURL
+	cache[fullURL] = shortURL
+	cacheBack[shortURL] = fullURL
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(shortUrl))
+	w.Write([]byte(shortURL))
 }
 
 func apiPage(w http.ResponseWriter, r *http.Request) {
-	shortUrl := "http://localhost:8080" + r.URL.String()
-	if fullUrl, ok := cacheBack[shortUrl]; ok {
-		w.Header().Set("Location", fullUrl)
+	shortURL := "http://localhost:8080" + r.URL.String()
+	if fullURL, ok := cacheBack[shortURL]; ok {
+		w.Header().Set("Location", fullURL)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 		return
 	}
-	log.Println(shortUrl)
+	log.Println(shortURL)
 	w.WriteHeader(400)
 }
 
