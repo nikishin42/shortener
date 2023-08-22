2de8bb43503f86f91fab3458209671e0428acc73
Nikishin Dmitry <dmitry.nikishin@technomos.ru>
2023-08-22 13:02:06 +0300

    server address and base shortener address can be configurated from env

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/nikishin42/shortener/cmd/shortener/constants"
 )
@@ -12,13 +13,32 @@ type Config struct {
 	BaseShortenerAddress string
 }
 
-func New() *Config {
+func parseFlags() (string, string) {
 	address := flag.String("a", constants.HostPrefix, "HTTP server start address")
 	baseShortenerAddress := flag.String("b", constants.HTTPHostPrefix, "base address of the resulting shortened URL")
 	flag.Parse()
 	log.Printf("flags parsed: -a: %s, -b: %s", *address, *baseShortenerAddress)
+	return *address, *baseShortenerAddress
+}
+
+func New() *Config {
+	a, b := parseFlags()
+	address, ok := os.LookupEnv("SERVER_ADDRESS")
+	if ok {
+		log.Printf("server address got from env: %s", address)
+	} else {
+		address = a
+		log.Printf("server address got from flag: %s", address)
+	}
+	baseShortenerAddress, ok := os.LookupEnv("BASE_URL")
+	if ok {
+		log.Printf("base shortener address got from env: %s", baseShortenerAddress)
+	} else {
+		baseShortenerAddress = b
+		log.Printf("base shortener address got from flag: %s", baseShortenerAddress)
+	}
 	return &Config{
-		Address:              *address,
-		BaseShortenerAddress: *baseShortenerAddress,
+		Address:              address,
+		BaseShortenerAddress: baseShortenerAddress,
 	}
 }
