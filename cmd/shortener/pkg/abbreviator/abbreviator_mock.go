a1d98b7c5f4f7190c4bfdf77debbeb70fc401850
Nikishin Dmitry <dmitry.nikishin@technomos.ru>
2023-08-22 12:46:09 +0300

    mock updated

--- cmd/shortener/pkg/abbreviator/abbreviator_mock.go
+++ cmd/shortener/pkg/abbreviator/abbreviator_mock.go
@@ -34,16 +34,16 @@ func (m *MockAbbreviatorI) EXPECT() *MockAbbreviatorIMockRecorder {
 }
 
 // CreateID mocks base method.
-func (m *MockAbbreviatorI) CreateID(arg0 []byte) (string, error) {
+func (m *MockAbbreviatorI) CreateID(arg0 []byte, arg1 string) (string, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "CreateID", arg0)
+	ret := m.ctrl.Call(m, "CreateID", arg0, arg1)
 	ret0, _ := ret[0].(string)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
 // CreateID indicates an expected call of CreateID.
-func (mr *MockAbbreviatorIMockRecorder) CreateID(arg0 interface{}) *gomock.Call {
+func (mr *MockAbbreviatorIMockRecorder) CreateID(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateID", reflect.TypeOf((*MockAbbreviatorI)(nil).CreateID), arg0)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateID", reflect.TypeOf((*MockAbbreviatorI)(nil).CreateID), arg0, arg1)
 }
