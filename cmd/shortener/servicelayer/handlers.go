8d5643d95e67bf340aa6b042f639e8e1786a6ba1
Nikishin Dmitry <dmitry.nikishin@technomos.ru>
2023-09-08 06:13:06 +0300

    add zap logger

--- cmd/shortener/servicelayer/handlers.go
+++ cmd/shortener/servicelayer/handlers.go
@@ -2,7 +2,6 @@ package servicelayer
 
 import (
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 
@@ -13,53 +12,54 @@ func (s *Server) Homepage(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if len(r.URL.Path) > 1 {
-		log.Print("query not empty")
+		s.Logger.Infof("path not empty")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	bodyData, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Print(err)
+		s.Logger.Error(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	fullURL := string(bodyData)
 	_, err = url.ParseRequestURI(fullURL)
 	if err != nil {
-		log.Print(err)
+		s.Logger.Error(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	id, fromCache, err := businesslayer.GetOrCreateID(s.Storage, s.Abbreviator, bodyData, s.Config.BaseShortenerAddress)
 	if err != nil {
-		log.Print(err)
+		s.Logger.Error(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	if fromCache {
-		log.Printf("ID for %s found: %s", fullURL, id)
+		s.Logger.Infof("ID for %s found: %s", fullURL, id)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(id))
 		return
 	}
+	s.Logger.Infof("ID for %s created: %s", fullURL, id)
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(id))
 }
 
 func (s *Server) Redirect(w http.ResponseWriter, r *http.Request) {
 	if len(r.URL.Path) < 2 {
-		log.Print("empty query")
+		s.Logger.Infof("empty path")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	id := r.URL.Path[1:]
 	fullURL, err := businesslayer.GetFullAddress(s.Config.BaseShortenerAddress, id, s.Storage)
 	if err != nil {
-		log.Print(err)
+		s.Logger.Info(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	log.Printf("URL for %s found: %s", id, fullURL)
+	s.Logger.Infof("URL for %s found: %s", id, fullURL)
 	w.Header().Set("Location", fullURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
